b617fa7d9af2016e00e088e8bac856e34d640319
Mihai H <59961959+automatica-cluj@users.noreply.github.com>
2022-02-19 12:16:02 +0200

    Exemplu String

--- isp-lab-2-master/src/main/java/isp/lab2/Example.java
+++ isp-lab-2-master/src/main/java/isp/lab2/Example.java
@@ -10,6 +10,15 @@ public class Example {
 
     public static Logger LOG = Logger.getLogger(Example.class.getName());
 
+    public static void workWithString(){
+        String s = "calculator,tema,laborator";
+
+        String[] result = s.split(",");
+
+        for(int i=0;i<result.length;i++){
+            System.out.println("first char="+result[i].charAt(0));
+        }
+    }
     /**
      * Add x and y.
      *
@@ -26,11 +35,14 @@ public class Example {
     }
 
     public static void main(String[] args) {
-        System.out.println("It works!");
-        LOG.info("It works!");
-        System.out.println("Number of arguments received: " + args.length + " ");
-        for (String arg : args)
-            System.out.println("Argument: " + arg);
-        System.out.printf("Result of adding %d with %d is: %d\n\n", 10, 23, Example.add(10, 23));
+
+workWithString();
+
+//        System.out.println("It works!");
+//        LOG.info("It works!");
+//        System.out.println("Number of arguments received: " + args.length + " ");
+//        for (String arg : args)
+//            System.out.println("Argument: " + arg);
+//        System.out.printf("Result of adding %d with %d is: %d\n\n", 10, 23, Example.add(10, 23));
     }
 }
